7aae09c22b1dc4a78625cf3a662761150a5945d2
MH <mudassar@1-platform.com>
2024-12-06 17:57:18 +0500

    chore: updating commitlint configs

---
+++ commitlint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      1, // Error level
+      'always',
+      [
+        'feat', // A new feature
+        'fix', // A bug fix
+        'docs', // Documentation changes
+        'style', // Code style changes
+        'refactor', // Code restructuring without functional changes
+        'perf', // Performance improvements
+        'test', // Adding or fixing tests
+        'build', // Build system changes
+        'ci', // CI/CD configuration changes
+        'chore', // Maintenance changes
+        'revert', // Reverting commits
+      ],
+    ],
+  },
+};
