db9189344fdc76b1b6c83e3ddcfeed453857dee0
MH <mudassar@1-platform.com>
2024-12-06 17:38:21 +0500

    add conventional commits and pre-staged hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import reactLogo from 'assets/react.svg';
+import ViteLogo from 'assets/vite.svg?react';
 import Test from 'components/test';
 
 import './App.css';
