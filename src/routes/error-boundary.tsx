31c4a95d8f521dc6250aea057464920ed043c3aa
MH <mudassar@1-platform.com>
2024-12-09 15:01:00 +0500

    chore: add router, antd, tailwind

--- src/routes/error-boundary.tsx
+++ src/routes/error-boundary.tsx
@@ -3,6 +3,8 @@ import { useRouteError } from 'react-router-dom';
 function ErrorBoundary() {
   const error = useRouteError() as Error;
 
+  console.log('ErrorBoundary', error);
+
   return <div>{error.message}</div>;
 }
 
